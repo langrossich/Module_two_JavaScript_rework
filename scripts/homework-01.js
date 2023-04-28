332d64d5b333e4af305595ebfdded3e921267612
Aleksandr R <langrossich@gmail.com>
2023-04-28 15:00:00 +0800

    Домашняя работа (14)

---
+++ scripts/homework-01.js
@@ -0,0 +1,14 @@
+console.log('-*-*-*-*-* homework-01 -*-*-*-*-*');
+let money = 60000; // Месячный бюджет
+let profit = "Фриланс"; // Cтрока с дополнительным доходом
+let expendes = "Еда, Такси, Развлечения"; // Cтрока с дополнительными расходами через запятую
+let purpose = "25000"; // Любое число (Какую сумму хотите накопить)
+let period = 12; // число от 1 до 12 (месяцев)
+
+console.log(typeof money, typeof profit)
+console.log(expendes.length)
+console.log(`Период равен ${period}-ти месяцам`);
+console.log(`Цель заработать ${purpose} рублей`);
+let budgetDay = money / 30;
+
+console.log(budgetDay);
